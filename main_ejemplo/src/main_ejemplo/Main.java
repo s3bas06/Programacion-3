1c911b037d06992caaa347005cff7c9e9b208e71
s3bas06 <152337109+s3bas06@users.noreply.github.com>
2024-02-20 09:57:03 -0700

    Unidad 2: Actividad 13
    Actividad 13. Titulo Jlabel

--- main_ejemplo/src/main_ejemplo/Main.java
+++ main_ejemplo/src/main_ejemplo/Main.java
@@ -1,19 +1,12 @@
 package main_ejemplo;
 
-import javax.swing.JFrame;
-
 public class Main {
 
 	public static void main(String[] args) {
 		
-		JFrame ventana = new JFrame("Example");
-		ventana.setVisible(true);
-		ventana.setSize(700,900);
-		ventana.setLocation(50,50);
+		ClaseVentana v1 = new ClaseVentana();
 		
-		ventana.setResizable(true);
-		
-		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		v1.setVisible(true);
 		
 		System.out.print("hola");
 	}
