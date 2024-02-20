07a8efb50bab3ae52f6ea4a5468e8440e103902b
s3bas06 <152337109+s3bas06@users.noreply.github.com>
2024-02-19 22:40:19 -0700

    unidad 2: ejercicio 10

---
+++ Interfaces_Graficas/src/Ventanas/Main.java
@@ -0,0 +1,12 @@
+package Ventanas;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ClaseVentana v1 = new ClaseVentana();
+		
+		v1.setVisible(true);
+	}
+
+}
